f3fa64e146216c872dec72d77fde2a5c8cedcfde
Yuki Yamada <yamadayuuki0818@gmail.com>
2015-08-11 15:26:46 +0900

    Create thread store

--- src/components/ThreadSection.jsx
+++ src/components/ThreadSection.jsx
@@ -1,23 +1,33 @@
 import React from "react";
+import ThreadStore from "../stores/ThreadStore.js";
+import ListenerMixin from "../mixins/ListenerMixin.js";
 
 function getStateFromStores() {
-  console.log("Return datas");
-  return null;
+  return {
+    threads: ThreadStore.getAllChrono(),
+    currentThreadID: ThreadStore.getCurrentID()
+  };
 }
 
 let ThreadSection = React.createClass({
+  mixins: [ListenerMixin],
+
   getInitialState() {
     return getStateFromStores();
   },
 
   componentDidMount() {
-
+    this.listenTo(ThreadStore, this._onChange);
   },
 
   render() {
     return (
       <div className="thread-section"><h2>ThreadSection</h2></div>
     );
+  },
+
+  _onChange() {
+    this.setState(getStateFromStores());
   }
 });
 
