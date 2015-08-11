7feaee8099cd35393c149baa4d215d21abc433a6
Yuki Yamada <yamadayuuki0818@gmail.com>
2015-08-11 22:13:37 +0900

    Add Example data

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import ChatExampleData from "./ChatExampleData.js";
 import ChatApp from "./components/ChatApp.jsx";
+import ChatWebAPIUtils from "./utils/ChatWebAPIUtils.js";
+
+ChatExampleData.init();
+
+ChatWebAPIUtils.getAllMessages();
 
 React.render(
   <ChatApp></ChatApp>, document.getElementById("react")
