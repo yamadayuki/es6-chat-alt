cc89d31a7d4b55287dc326dd532404060f4d9bec
Yuki Yamada <yamadayuuki0818@gmail.com>
2015-08-11 14:26:01 +0900

    First integration
      enable to display "ChatApp" string.
      I can use es6 (babel) to create React app.

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import App from "./components/App.jsx";
+import ChatApp from "./components/ChatApp.jsx";
 
 React.render(
-  <App></App>, document.getElementById("react")
+  <ChatApp></ChatApp>, document.getElementById("react")
 );
---
+++ src/components/ChatApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class ChatApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>ChatApp</h1>
+    );
+  }
+}
